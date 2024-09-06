52460a996dbaac2a708bd2128193e801cfb8ba64
hiroaki.kitano <hiroaki.kitano@appbank.net>
2024-09-06 14:31:45 +0900

    add_routes

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Flex } from '@chakra-ui/react';
 import Example from './components/Example';
 
-const rootElement = document.getElementById('app');
-
-if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(<Example />);
+const RouteContents = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Example />} /> {/* Exampleページ */}
+        </Routes>
+    );
 }
+
+function App() {
+    return (
+        <Router>
+            <Flex>
+                <RouteContents />
+            </Flex>
+        </Router>
+    );
+}
+
+// レンダリング部分
+const root = ReactDOM.createRoot(document.getElementById('app')!);
+root.render(<App />);
